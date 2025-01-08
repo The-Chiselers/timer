5ca338caf86b46ca554c9ee0492a2fa17b542ceb
Nick Allison <nickrallison1@gmail.com>
2025-01-08 00:07:50 -0700

    added cases to main file

--- src/main/scala/tech/rocksavage/Main.scala
+++ src/main/scala/tech/rocksavage/Main.scala
@@ -4,6 +4,7 @@ import chisel3._
 
 import java.io.File
 import tech.rocksavage.args.Conf
+import tech.rocksavage.chiselware.timer.param.TimerParams
 import tech.rocksavage.synth.Synth.{genVerilogFromModuleName, synthesizeFromModuleName}
 
 import scala.sys.exit
@@ -11,39 +12,56 @@ import scala.sys.exit
 object Main {
   def main(args_array: Array[String]): Unit = {
     val conf = new Conf(args_array.toIndexedSeq)
+
+    // Define multiple default configurations
+    val defaultConfigs = Map(
+      "config1" -> TimerParams(dataWidth = 32, addressWidth = 32, countWidth = 32),
+      "config2" -> TimerParams(dataWidth = 16, addressWidth = 16, countWidth = 16),
+      "config3" -> TimerParams(dataWidth = 64, addressWidth = 64, countWidth = 64),
+      "config4" -> TimerParams(dataWidth = 8, addressWidth = 8, countWidth = 8)
+    )
+
     conf.subcommand match {
       case Some(conf.verilog) => {
-        val verilogString = genVerilogFromModuleName(conf.verilog.module())
-        conf.verilog.mode() match {
-          case "print" => {
-            println(verilogString)
-          }
-          case "write" => {
-            val filename = conf.verilog.module() + ".sv"
-            // write to file
-            def f = new File(filename)
-            val bw = new java.io.BufferedWriter(new java.io.FileWriter(f))
-            bw.write(verilogString)
+        // Run Verilog generation for each configuration
+        defaultConfigs.foreach { case (name, params) =>
+          println(s"Generating Verilog for configuration: $name")
+          val verilogString = genVerilogFromModuleName(conf.verilog.module(), params)
+          conf.verilog.mode() match {
+            case "print" => {
+              println(verilogString)
+            }
+            case "write" => {
+              val filename = s"${conf.verilog.module()}_$name.sv"
+              val f = new File(filename)
+              val bw = new java.io.BufferedWriter(new java.io.FileWriter(f))
+              bw.write(verilogString)
+              bw.close()
+            }
           }
         }
       }
       case Some(conf.synth) => {
-        val synthCommands = List(
-          tech.rocksavage.synth.SynthCommand.Synth,
-          tech.rocksavage.synth.SynthCommand.Flatten,
-          tech.rocksavage.synth.SynthCommand.Dfflibmap,
-          tech.rocksavage.synth.SynthCommand.Abc,
-          tech.rocksavage.synth.SynthCommand.Opt,
-          tech.rocksavage.synth.SynthCommand.Clean,
-          tech.rocksavage.synth.SynthCommand.Stat
-        )
-        val synthConfig = new tech.rocksavage.synth.SynthConfig(
-          conf.synth.techlib(),
-          synthCommands
-        )
-        val synth = synthesizeFromModuleName(synthConfig, conf.synth.module())
-        println(synth.getStdout)
-        println(synth.getSynthString)
+        // Run synthesis for each configuration
+        defaultConfigs.foreach { case (name, params) =>
+          println(s"Synthesizing configuration: $name")
+          val synthCommands = List(
+            tech.rocksavage.synth.SynthCommand.Synth,
+            tech.rocksavage.synth.SynthCommand.Flatten,
+            tech.rocksavage.synth.SynthCommand.Dfflibmap,
+            tech.rocksavage.synth.SynthCommand.Abc,
+            tech.rocksavage.synth.SynthCommand.Opt,
+            tech.rocksavage.synth.SynthCommand.Clean,
+            tech.rocksavage.synth.SynthCommand.Stat
+          )
+          val synthConfig = new tech.rocksavage.synth.SynthConfig(
+            conf.synth.techlib(),
+            synthCommands
+          )
+          val synth = synthesizeFromModuleName(synthConfig, conf.synth.module(), params)
+          println(synth.getStdout)
+          println(synth.getSynthString)
+        }
       }
       case _ => {
         println("No subcommand given")
@@ -51,4 +69,3 @@ object Main {
     }
   }
 }
-
