894a48a88793c34c1ea17147f6ad7e301189cdc3
Nick Allison <nickrallison1@gmail.com>
2025-01-11 14:42:34 -0700

    updated run config names

--- src/main/scala/tech/rocksavage/chiselware/timer/TimerConfig.scala
+++ src/main/scala/tech/rocksavage/chiselware/timer/TimerConfig.scala
@@ -5,9 +5,9 @@ import tech.rocksavage.chiselware.timer.param.TimerParams
 
 class TimerConfig extends ConfigTrait {
   override def getDefaultConfigs: Map[String, Any] = Map(
-    "config1" -> TimerParams(dataWidth = 8, addressWidth = 8, countWidth = 8),
-    "config2" -> TimerParams(dataWidth = 16, addressWidth = 16, countWidth = 16),
-    "config3" -> TimerParams(dataWidth = 32, addressWidth = 32, countWidth = 32),
-    "config4" -> TimerParams(dataWidth = 64, addressWidth = 64, countWidth = 64)
+    "8_8_8" -> TimerParams(dataWidth = 8, addressWidth = 8, countWidth = 8),
+    "16_16_16" -> TimerParams(dataWidth = 16, addressWidth = 16, countWidth = 16),
+    "32_32_32" -> TimerParams(dataWidth = 32, addressWidth = 32, countWidth = 32),
+    "64_64_64" -> TimerParams(dataWidth = 64, addressWidth = 64, countWidth = 64)
   )
 }
