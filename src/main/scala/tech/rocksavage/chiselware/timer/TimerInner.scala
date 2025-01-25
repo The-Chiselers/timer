54705eb4c5b2368a253b26092a549aa667f28935
Nick Allison <nickrallison1@gmail.com>
2025-01-25 11:52:56 -0700

    reorganizing a bit

--- src/main/scala/tech/rocksavage/chiselware/timer/TimerInner.scala
+++ src/main/scala/tech/rocksavage/chiselware/timer/TimerInner.scala
@@ -3,7 +3,6 @@
 package tech.rocksavage.chiselware.timer
 
 import chisel3._
-import chiseltest.formal.past
 import tech.rocksavage.chiselware.timer.bundle.TimerBundle
 import tech.rocksavage.chiselware.timer.param.TimerParams
 
@@ -23,120 +22,122 @@ class TimerInner(
     formal: Boolean = false
 ) extends Module {
 
-    /** Returns the number of memory addresses used by the module
-      *
-      * @return
-      *   The width of the memory
-      */
-    val io = IO(new TimerBundle(params))
+  /** Returns the number of memory addresses used by the module
+    *
+    * @return
+    *   The width of the memory
+    */
+  val io = IO(new TimerBundle(params))
 
-    // ###################
-    // Syncronizers for input / formal verify
-    // ###################
-    val enReg            = RegInit(false.B)
-    val prescalerReg     = RegInit(0.U(params.countWidth.W))
-    val maxCountReg      = RegInit(0.U(params.countWidth.W))
-    val pwmCeilingReg    = RegInit(0.U(params.countWidth.W))
-    val setCountValueReg = RegInit(0.U(params.countWidth.W))
-    val setCountReg      = RegInit(false.B)
+  // ###################
+  // Syncronizers for input / formal verify
+  // ###################
+  val enReg = RegInit(false.B)
+  val prescalerReg = RegInit(0.U(params.countWidth.W))
+  val maxCountReg = RegInit(0.U(params.countWidth.W))
+  val pwmCeilingReg = RegInit(0.U(params.countWidth.W))
+  val setCountValueReg = RegInit(0.U(params.countWidth.W))
+  val setCountReg = RegInit(false.B)
 
-    // ###################
-    // Registers that hold the output values
-    // ###################
-    val countReg      = RegInit(0.U(params.countWidth.W))
-    val maxReachedReg = RegInit(false.B)
-    val pwmReg        = RegInit(false.B)
+  // ###################
+  // Registers that hold the output values
+  // ###################
+  val countReg = RegInit(0.U(params.countWidth.W))
+  val maxReachedReg = RegInit(false.B)
+  val pwmReg = RegInit(false.B)
 
-    // ###################
-    // Next state logic
-    // ###################
-    val nextCount      = WireInit(0.U(params.countWidth.W))
-    val nextMaxReached = WireInit(false.B)
-    val nextPwm        = WireInit(false.B)
+  // ###################
+  // Next state logic
+  // ###################
+  val nextCount = WireInit(0.U(params.countWidth.W))
+  val nextMaxReached = WireInit(false.B)
+  val nextPwm = WireInit(false.B)
 
-    // ###################
-    // Instatiation
-    // ###################
+  // ###################
+  // Instatiation
+  // ###################
 
-    enReg            := io.timerInputBundle.en
-    prescalerReg     := io.timerInputBundle.prescaler
-    maxCountReg      := io.timerInputBundle.maxCount
-    pwmCeilingReg    := io.timerInputBundle.pwmCeiling
-    setCountValueReg := io.timerInputBundle.setCountValue
-    setCountReg      := io.timerInputBundle.setCount
+  enReg := io.timerInputBundle.en
+  prescalerReg := io.timerInputBundle.prescaler
+  maxCountReg := io.timerInputBundle.maxCount
+  pwmCeilingReg := io.timerInputBundle.pwmCeiling
+  setCountValueReg := io.timerInputBundle.setCountValue
+  setCountReg := io.timerInputBundle.setCount
 
-    countReg      := nextCount
-    maxReachedReg := nextMaxReached
-    pwmReg        := nextPwm
+  countReg := nextCount
+  maxReachedReg := nextMaxReached
+  pwmReg := nextPwm
 
-    // ###################
-    // Output
-    // ###################
-    io.timerOutputBundle.count      := countReg
-    io.timerOutputBundle.maxReached := maxReachedReg
-    io.timerOutputBundle.pwm        := pwmReg
+  // ###################
+  // Output
+  // ###################
+  io.timerOutputBundle.count := countReg
+  io.timerOutputBundle.maxReached := maxReachedReg
+  io.timerOutputBundle.pwm := pwmReg
 
-    // ###################
-    // Module implementation
-    // ###################
+  // ###################
+  // Module implementation
+  // ###################
 
-    val countSum = WireInit(0.U((params.countWidth).W))
+  val countSum = WireInit(0.U((params.countWidth).W))
+  countSum := countReg + prescalerReg
+
+  val countOverflow = WireInit(false.B)
+  countOverflow := (countSum < countReg) || (countSum < prescalerReg)
+
+  // Combinational logic
+  when(setCountReg) {
+    countSum := setCountValueReg
+  }.otherwise {
     countSum := countReg + prescalerReg
+  }
 
-    val countOverflow = WireInit(false.B)
+  when(setCountReg) {
+    countOverflow := false.B
+  }.otherwise {
     countOverflow := (countSum < countReg) || (countSum < prescalerReg)
+  }
 
-    when(enReg) {
+  // reg logic
+  when(enReg) {
 
-        val countSum = WireInit(0.U((params.countWidth).W))
-        when(setCountReg) {
-            countSum := setCountValueReg
-        }.otherwise {
-            countSum := countReg + prescalerReg
-        }
-
-        val countOverflow = WireInit(false.B)
-        when(setCountReg) {
-            countOverflow := false.B
-        }.otherwise {
-            countOverflow := (countSum < countReg) || (countSum < prescalerReg)
-        }
-
-        // If the countSum is greater than the maxCount, then reset the count to 0, the max has been reached
-        // also reset the count if the countSum overflows, this means that the maxMust be reached as well
-        when(countSum >= maxCountReg || countOverflow) {
-            nextCount      := 0.U
-            nextMaxReached := true.B
-        }.otherwise {
-            nextCount      := countReg + prescalerReg
-            nextMaxReached := false.B
-        }
-        nextPwm := countReg >= pwmCeilingReg
+    // If the countSum is greater than the maxCount, then reset the count to 0, the max has been reached
+    // also reset the count if the countSum overflows, this means that the maxMust be reached as well
+    when(countSum >= maxCountReg || countOverflow) {
+      nextCount := 0.U
+      nextMaxReached := true.B
     }.otherwise {
-        nextCount      := countReg
-        nextMaxReached := maxReachedReg
-        nextPwm        := pwmReg
+      nextCount := countReg + prescalerReg
+      nextMaxReached := false.B
     }
+    nextPwm := countReg >= pwmCeilingReg
+  }.otherwise {
 
-    // ###################
-    // Formal verification
-    // ###################
-    if (formal) {
-        // Formal Verification Vars
-        when(enReg) {
-            // ######################
-            // Liveness Specification
-            // ######################
+    // Regs
+    nextCount := countReg
+    nextMaxReached := maxReachedReg
+    nextPwm := pwmReg
+  }
 
-            // assert that every cycle,
-            // (prescaler > 0) implies ((nextCount > countReg) or (nextMaxReached))
+  // ###################
+  // Formal verification
+  // ###################
+  if (formal) {
+    // Formal Verification Vars
+    when(enReg) {
+      // ######################
+      // Liveness Specification
+      // ######################
 
-            val madeProgressFV = (nextCount > countReg)
-            val maxReachedFV   = nextMaxReached
+      // assert that every cycle,
+      // (prescaler > 0) implies ((nextCount > countReg) or (nextMaxReached))
 
-            when(prescalerReg > 0.U && !setCountReg) {
-                assert(madeProgressFV || maxReachedFV)
-            }
-        }
+      val madeProgressFV = (nextCount > countReg)
+      val maxReachedFV = nextMaxReached
+
+      when(prescalerReg > 0.U && !setCountReg) {
+        assert(madeProgressFV || maxReachedFV)
+      }
     }
+  }
 }
--- src/test/scala/tech/rocksavage/chiselware/timer/TimerTest.scala
+++ src/test/scala/tech/rocksavage/chiselware/timer/TimerTest.scala
@@ -10,106 +10,102 @@ import firrtl2.TargetDirAnnotation
 import firrtl2.annotations.Annotation
 import org.scalatest.flatspec.AnyFlatSpec
 import org.scalatest.matchers.should.Matchers.convertToAnyShouldWrapper
-import tech.rocksavage.chiselware.apb.ApbBundle
 import tech.rocksavage.chiselware.apb.ApbTestUtils._
-import tech.rocksavage.chiselware.addressable.RegisterMap
-import tech.rocksavage.chiselware.timer.Timer
-import tech.rocksavage.chiselware.timer.bundle.TimerInterruptEnum
 import tech.rocksavage.chiselware.timer.param.TimerParams
 
 class TimerTest extends AnyFlatSpec with ChiselScalatestTester {
-    val verbose  = false
-    val numTests = 2
-    val testName = System.getProperty("testName")
-    println(s"Argument passed: $testName")
+  val verbose = false
+  val numTests = 2
+  val testName = System.getProperty("testName")
+  println(s"Argument passed: $testName")
 
-    // System properties for flags
-    val enableVcd    = System.getProperty("enableVcd", "false").toBoolean
-    val enableFst    = System.getProperty("enableFst", "true").toBoolean
-    val useVerilator = System.getProperty("useVerilator", "false").toBoolean
+  // System properties for flags
+  val enableVcd = System.getProperty("enableVcd", "false").toBoolean
+  val enableFst = System.getProperty("enableFst", "true").toBoolean
+  val useVerilator = System.getProperty("useVerilator", "false").toBoolean
 
-    val buildRoot = "out"
-    val testDir   = buildRoot + "/test"
+  val buildRoot = "out"
+  val testDir = buildRoot + "/test"
 
-    println(
-      s"Test: $testName, VCD: $enableVcd, FST: $enableFst, Verilator: $useVerilator"
-    )
+  println(
+    s"Test: $testName, VCD: $enableVcd, FST: $enableFst, Verilator: $useVerilator"
+  )
 
-    // Constructing the backend annotations based on the flags
-    val backendAnnotations = {
-        var annos: Seq[Annotation] = Seq() // Initialize with correct type
+  // Constructing the backend annotations based on the flags
+  val backendAnnotations = {
+    var annos: Seq[Annotation] = Seq() // Initialize with correct type
 
-        if (enableVcd) annos = annos :+ chiseltest.simulator.WriteVcdAnnotation
-        if (enableFst) annos = annos :+ chiseltest.simulator.WriteFstAnnotation
-        if (useVerilator) {
-            annos = annos :+ chiseltest.simulator.VerilatorBackendAnnotation
-            annos = annos :+ VerilatorCFlags(Seq("--std=c++17"))
-        }
-        annos = annos :+ TargetDirAnnotation(testDir)
-
-        annos
+    if (enableVcd) annos = annos :+ chiseltest.simulator.WriteVcdAnnotation
+    if (enableFst) annos = annos :+ chiseltest.simulator.WriteFstAnnotation
+    if (useVerilator) {
+      annos = annos :+ chiseltest.simulator.VerilatorBackendAnnotation
+      annos = annos :+ VerilatorCFlags(Seq("--std=c++17"))
     }
+    annos = annos :+ TargetDirAnnotation(testDir)
 
-    val timerParams = TimerParams(
-      dataWidth = 32,
-      addressWidth = 32,
-      countWidth = 64
-    )
+    annos
+  }
 
-    "Timer" should "correctly handle register writes and reads" in {
-        test(new Timer(timerParams)).withAnnotations(backendAnnotations) {
-            dut =>
-                implicit val clock = dut.clock
+  val timerParams = TimerParams(
+    dataWidth = 32,
+    addressWidth = 32,
+    countWidth = 64
+  )
 
-                // Get the register map from the Timer module
-                val registerMap = dut.registerMap
+  "Timer" should "correctly handle register writes and reads" in {
+    test(new Timer(timerParams, false)).withAnnotations(backendAnnotations) {
+      dut =>
+        implicit val clock = dut.clock
 
-                // Get the addresses of the registers
-                val enAddr = registerMap.getAddressOfRegister("en").get
-                val prescalerAddr =
-                    registerMap.getAddressOfRegister("prescaler").get
-                val maxCountAddr =
-                    registerMap.getAddressOfRegister("maxCount").get
-                val pwmCeilingAddr =
-                    registerMap.getAddressOfRegister("pwmCeiling").get
-                val setCountValueAddr =
-                    registerMap.getAddressOfRegister("setCountValue").get
-                val setCountAddr =
-                    registerMap.getAddressOfRegister("setCount").get
+        // Get the register map from the Timer module
+        val registerMap = dut.registerMap
 
-                // Write to the prescaler register
-                writeAPB(dut.io.apb, prescalerAddr.U, 10.U)
-                readAPB(dut.io.apb, prescalerAddr.U) shouldEqual 10
+        // Get the addresses of the registers
+        val enAddr = registerMap.getAddressOfRegister("en").get
+        val prescalerAddr =
+          registerMap.getAddressOfRegister("prescaler").get
+        val maxCountAddr =
+          registerMap.getAddressOfRegister("maxCount").get
+        val pwmCeilingAddr =
+          registerMap.getAddressOfRegister("pwmCeiling").get
+        val setCountValueAddr =
+          registerMap.getAddressOfRegister("setCountValue").get
+        val setCountAddr =
+          registerMap.getAddressOfRegister("setCount").get
 
-                // Write to the maxCount register
-                writeAPB(dut.io.apb, maxCountAddr.U, 1024.U)
-                readAPB(dut.io.apb, maxCountAddr.U) shouldEqual 1024
+        // Write to the prescaler register
+        writeAPB(dut.io.apb, prescalerAddr.U, 10.U)
+        readAPB(dut.io.apb, prescalerAddr.U) shouldEqual 10
 
-                // Write to the pwmCeiling register
-                writeAPB(dut.io.apb, pwmCeilingAddr.U, 50.U)
-                readAPB(dut.io.apb, pwmCeilingAddr.U) shouldEqual 50
+        // Write to the maxCount register
+        writeAPB(dut.io.apb, maxCountAddr.U, 1024.U)
+        readAPB(dut.io.apb, maxCountAddr.U) shouldEqual 1024
 
-                // Write to the setCountValue register
-                writeAPB(dut.io.apb, setCountValueAddr.U, 20.U)
-                readAPB(dut.io.apb, setCountValueAddr.U) shouldEqual 20
+        // Write to the pwmCeiling register
+        writeAPB(dut.io.apb, pwmCeilingAddr.U, 50.U)
+        readAPB(dut.io.apb, pwmCeilingAddr.U) shouldEqual 50
 
-                // Write to the setCount register
-                writeAPB(dut.io.apb, setCountAddr.U, 1.U)
-                readAPB(dut.io.apb, setCountAddr.U) shouldEqual 1
+        // Write to the setCountValue register
+        writeAPB(dut.io.apb, setCountValueAddr.U, 20.U)
+        readAPB(dut.io.apb, setCountValueAddr.U) shouldEqual 20
 
-                // Write to the enable register
-                writeAPB(dut.io.apb, enAddr.U, 1.U)
-                readAPB(dut.io.apb, enAddr.U) shouldEqual 1
+        // Write to the setCount register
+        writeAPB(dut.io.apb, setCountAddr.U, 1.U)
+        readAPB(dut.io.apb, setCountAddr.U) shouldEqual 1
 
-                // Step the clock until the count reaches maxCount
-                while (dut.io.timerOutput.count.peekInt() < 1000) {
-                    dut.clock.step(1)
-                }
+        // Write to the enable register
+        writeAPB(dut.io.apb, enAddr.U, 1.U)
+        readAPB(dut.io.apb, enAddr.U) shouldEqual 1
 
-                // Check that maxReached is true
-                dut.io.timerOutput.pwm.expect(
-                  true.B
-                ) // Since count >= pwmCeiling, PWM should be high
+        // Step the clock until the count reaches maxCount
+        while (dut.io.timerOutput.count.peekInt() < 1000) {
+          dut.clock.step(1)
         }
+
+        // Check that maxReached is true
+        dut.io.timerOutput.pwm.expect(
+          true.B
+        ) // Since count >= pwmCeiling, PWM should be high
     }
+  }
 }
